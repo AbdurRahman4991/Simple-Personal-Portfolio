d4b6e6d45141c66ab9564b7e87e8b0920b2101cd
AbdurRahman4991 <engrabdurrahman4991@gmail.com>
2024-08-21 10:49:59 +0600

    Add request request and model configerrtion

--- Backend/app/Models/About.php
+++ Backend/app/Models/About.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class About extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'description',
+        'address',
+        'skill',
+    ];
 }
